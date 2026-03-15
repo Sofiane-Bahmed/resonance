e67c84c2ad07c3476e3834064da07cff1aafd771
sofianso24 <sofiane.bahmed.fabrikademy@gmail.com>
2026-03-15 10:49:10 +0100

    voice selection

--- src/app/(dashboard)/text-to-speech/page.tsx
+++ src/app/(dashboard)/text-to-speech/page.tsx
@@ -1,9 +1,21 @@
 import { Metadata } from "next";
+import { trpc, prefetch, HydrateClient } from "@/trpc/server";
 
 import { TextToSpeechView } from "@/features/text-to-speech/views/text-to-speech-view";
 
 export const metadata: Metadata = { title: "Text to speech" }
 
-export default function TextToSpeech() {
-    return <TextToSpeechView />
-}
+export default async function TextToSpeech({
+    searchParams
+}: {
+    searchParams: Promise<{ text?: string, voiceId?: string }>
+}) {
+    const { text, voiceId } = await searchParams;
+    prefetch(trpc.voices.getAll.queryOptions());
+
+    return (
+        <HydrateClient>
+            <TextToSpeechView initialValues={{ text, voiceId }} />
+        </HydrateClient>
+    );
+};
